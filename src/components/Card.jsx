4ab9932b56177752a4fe589fdc8ef84a99bc3239
ceejeey <muhammed030@gmail.com>
2022-01-11 12:11:13 +0530

    second commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,50 @@ import Button from './Button'
 import * as style from './Content_style.css'
 import Tag from './Tag'
 
+import { useState, useEffect } from "react"
 
+const GITHUB_CLIENT_ID = "284633ed1aa09d0f2c16";
+
+const downloadFromAnchor = (resource_link) => {
+  const link = document.createElement("a");
+  link.href = resource_link;
+
+  // Append to html link element page
+  document.body.appendChild(link);
+
+  // Start download
+  link.click();
+
+  // Clean up and remove the link
+  link.parentNode.removeChild(link);
+};
 function Card(props) {
+    
+    
 
     const{description,title,tags} = props
+
+    const [templateName, setTemplateName] = useState("my project");
+
+    const [template, settemplate] = useState(title);
+
+  
+   
+    const templatetitle = (index) => {
+        settemplate(index)
+        console.log(template)
+        const url = `https://github.com/ceejeey/${template}/archive/refs/heads/main.zip`;
+        downloadFromAnchor(url);
+        
+    };
+
+
+       
+    const onClickGithub = (index) => {
+        settemplate(index)
+        const gitHubRedirectURL = `https://52zqd4fqic.execute-api.us-west-2.amazonaws.com/prod/github?data=${template},${templateName},repo`;
+        window.location.href = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${gitHubRedirectURL}&scope=repo`;
+      };
 
     return (
         <>
@@ -18,11 +58,11 @@ function Card(props) {
                  </div>
                  <div className={style.IconContainer}>
                     <div className={style.IconWrap}>
-                         <div className={style.Iconbackground}>
+                         <div className={style.Iconbackground} onClick={() => onClickGithub(title)}>
                              <i class="fab fa-github fa-1x"></i>    
                         </div>
-                        <div className={style.Iconbackground}>
-                             <i class="fas fa-download fa-1x"></i>
+                        <div className={style.Iconbackground} onClick={() => templatetitle(title)}>
+                            <i class="fas fa-download fa-1x"></i> 
                         </div>
                     </div>
                  </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,47 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Card from './Card'
-import { useState } from "react"
+
+import { useState, useEffect } from "react";
+
+
+import axios from "axios";
 
 import * as style from './Home_style.css'
 
+
+
+
 function Home() {
 
+
+
     const [toggleState, setToggleState] = useState(1);
+
 
     return (
         
         <div className={style.Container}>
             <Navbar setToggleState={setToggleState} toggleState={toggleState}/>
+
             <div className={toggleState === 1 ? style.ActiveContent : style.Content}>
             <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-             when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="All" tags={["emotion","react"]}/> 
-      <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+             when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="react-base" tags={["emotion","react"]}/> 
+            <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
              <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
             </div>
 
+
+
+
+
+
             <div className={toggleState === 2 ? style.ActiveContent : style.Content}>
             <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="Stylng" tags={["emotion","react"]}/> 
-      <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
              <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
@@ -34,7 +50,7 @@ function Home() {
             <div className={toggleState === 3 ? style.ActiveContent : style.Content}>
             <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
-      <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
              <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
@@ -43,7 +59,7 @@ function Home() {
             <div className={toggleState === 4 ? style.ActiveContent : style.Content}>
             <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
-      <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
              <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
@@ -52,7 +68,7 @@ function Home() {
             <div className={toggleState === 5 ? style.ActiveContent : style.Content}>
             <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
-      <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
              <Card description="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
              when an unknown printer took a galley of type and scrambled it to make a type specimen book." title="REACT-EMOTIONS-CSS" tags={["emotion","react"]}/> 
