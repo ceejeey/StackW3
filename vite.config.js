4f9810785530bcd556841c3b0c3163d0b1a4fa2a
ceejeey <muhammed030@gmail.com>
2022-01-14 12:30:23 +0530

    Redirection

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
-  rewrites: [{ source: '/(.*)', destination: '/index.html' }]
+  plugins: [react(), vanillaExtractPlugin()]
 });
