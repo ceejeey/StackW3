6f26af3691fedaaf81467954530bb8ce4506a238
ceejeey <muhammed030@gmail.com>
2022-01-28 16:53:00 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,40 +8,20 @@ function Navbar({ setToggleState, toggleState }) {
 
   return (
     <div className={style.AppHeader}>
-      <div className={style.Navmenu} variants={container} initial="hidden" animate="show" exit="exit">
-        <div
-          variants={item}
-          className={toggleState === 1 ? style.ActiveNavlink : style.Navlink}
-          onClick={() => toggleTab(1)}
-        >
+      <div className={style.Navmenu}>
+        <div className={toggleState === 1 ? style.ActiveNavlink : style.Navlink} onClick={() => toggleTab(1)}>
           All
         </div>
-        <div
-          variants={item}
-          className={toggleState === 2 ? style.ActiveNavlink : style.Navlink}
-          onClick={() => toggleTab(2)}
-        >
+        <div className={toggleState === 2 ? style.ActiveNavlink : style.Navlink} onClick={() => toggleTab(2)}>
           Styling
         </div>
-        <div
-          variants={item}
-          className={toggleState === 3 ? style.ActiveNavlink : style.Navlink}
-          onClick={() => toggleTab(3)}
-        >
+        <div className={toggleState === 3 ? style.ActiveNavlink : style.Navlink} onClick={() => toggleTab(3)}>
           State Managment
         </div>
-        <div
-          variants={item}
-          className={toggleState === 4 ? style.ActiveNavlink : style.Navlink}
-          onClick={() => toggleTab(4)}
-        >
+        <div className={toggleState === 4 ? style.ActiveNavlink : style.Navlink} onClick={() => toggleTab(4)}>
           Data Fetching
         </div>
-        <div
-          variants={item}
-          className={toggleState === 5 ? style.ActiveNavlink : style.Navlink}
-          onClick={() => toggleTab(5)}
-        >
+        <div className={toggleState === 5 ? style.ActiveNavlink : style.Navlink} onClick={() => toggleTab(5)}>
           Misc
         </div>
       </div>
